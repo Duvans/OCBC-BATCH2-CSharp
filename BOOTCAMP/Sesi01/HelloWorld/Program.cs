922628ba26f2ac8c5d578e8ac63ec349b7fdb9ae
Duvans <duvan.jayasan@gmail.com>
2021-11-22 14:57:55 +0700

    first commit

---
+++ BOOTCAMP/Sesi01/HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+
+// bermain git
+// ---
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git remote add origin https://github.com/Leonuch/flex.git
+// git checkout -b main
+// git pull origin main --allow-unrelated-histories
+// git push -u origin main
