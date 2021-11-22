0418bdf247d0b4900078a92f83781d448a23e03b
Duvans <duvan.jayasan@gmail.com>
2021-11-22 16:30:15 +0700

    sec commit

--- BOOTCAMP/Sesi01/HelloWorld/Program.cs
+++ BOOTCAMP/Sesi01/HelloWorld/Program.cs
@@ -10,14 +10,3 @@ namespace HelloWorld
         }
     }
 }
-
-
-// bermain git
-// ---
-// git init
-// git add README.md
-// git commit -m "first commit"
-// git remote add origin https://github.com/Leonuch/flex.git
-// git checkout -b main
-// git pull origin main --allow-unrelated-histories
-// git push -u origin main
---
+++ BOOTCAMP/Sesi02/HitungNilai4/HitungNilai4 - Live Code.cs
@@ -0,0 +1,25 @@
+using System;
+
+class HitungNilai4
+{
+    public static void Main(String[] args)
+    {
+        Console.Write("Enter your age: ");
+        int age = int.Parse(Console.ReadLine());
+        Console.Write("Password: ");
+        string password = Console.ReadLine();
+
+        bool isAdult = age > 18; //pernyataan1
+        bool isPasswordValid = password == "OCBC"; //pernyataan 2
+
+        //menggunakan logika AND
+        if(isAdult && isPasswordValid)
+        {
+            Console.WriteLine("WELCOME TO THE CLUB!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, try again!");
+        }
+    }
+}
